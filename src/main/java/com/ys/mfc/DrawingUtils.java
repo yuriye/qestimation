1ad6c809da105760f2ef9ba7acd2d910d63006be
Yuriy Yeliseyev <yuriye@mail.ru>
2018-03-20 17:48:47 +1200

    180320.2

--- src/main/java/com/ys/mfc/DrawingUtils.java
+++ src/main/java/com/ys/mfc/DrawingUtils.java
@@ -2,12 +2,8 @@ package com.ys.mfc;
 
 import com.WacomGSS.STU.Protocol.PenData;
 
-import javax.swing.*;
 import java.awt.*;
-import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
-import java.util.ArrayList;
-import java.util.List;
 
 public class DrawingUtils {
 
@@ -23,40 +19,40 @@ public class DrawingUtils {
         gfx.drawString(text, textX, textY);
     }
 
-//    static public void drawLongStringBySpliting(Graphics2D gfx, String text, int x,
-//                                                int y, int width, int height) {
-//        FontMetrics fm = gfx.getFontMetrics(gfx.getFont());
-//        int textHeight = fm.getHeight();
-//        int textWidth = fm.stringWidth(text);
-//        String head = "";
-//        String tail = "";
-//        int headWidth = textWidth;
-//        a:
-//        if (textWidth > width) {
-//            int endOfHeadPos = text.length();
-//
-//            while (headWidth >= width) {
-//                endOfHeadPos = text.lastIndexOf(" ", endOfHeadPos - 1);
-//                if (endOfHeadPos == -1) break a;
-//                head = text.substring(0, endOfHeadPos);
-//                headWidth = fm.stringWidth(head);
-//            }
-//            tail = text.substring(endOfHeadPos + 1);
-//        }
-//        int textX;
-//        int textY;
-//        if ("".equals(tail)) {
-//            textX = x + (width - textWidth) / 2;
-//            textY = y + (height - textHeight) / 2 + fm.getAscent();
-//            gfx.drawString(text, textX, textY);
-//        } else {
-//            textX = 8;
-//            textY = y + (height / 2 - textHeight) / 2 + fm.getAscent();
-//            gfx.drawString(head, textX, textY);
-//            textY = textY + height / 2;
-//            gfx.drawString(tail, textX, textY);
-//        }
-//    }
+    static public void drawLongStringBySpliting(Graphics2D gfx, String text, int x,
+                                                int y, int width, int height) {
+        FontMetrics fm = gfx.getFontMetrics(gfx.getFont());
+        int textHeight = fm.getHeight();
+        int textWidth = fm.stringWidth(text);
+        String head = "";
+        String tail = "";
+        int headWidth = textWidth;
+        a:
+        if (textWidth > width) {
+            int endOfHeadPos = text.length();
+
+            while (headWidth >= width) {
+                endOfHeadPos = text.lastIndexOf(" ", endOfHeadPos - 1);
+                if (endOfHeadPos == -1) break a;
+                head = text.substring(0, endOfHeadPos);
+                headWidth = fm.stringWidth(head);
+            }
+            tail = text.substring(endOfHeadPos + 1);
+        }
+        int textX;
+        int textY;
+        if ("".equals(tail)) {
+            textX = x + (width - textWidth) / 2;
+            textY = y + (height - textHeight) / 2 + fm.getAscent();
+            gfx.drawString(text, textX, textY);
+        } else {
+            textX = 8;
+            textY = y + (height / 2 - textHeight) / 2 + fm.getAscent();
+            gfx.drawString(head, textX, textY);
+            textY = textY + height / 2;
+            gfx.drawString(tail, textX, textY);
+        }
+    }
 
     private static String[] getHeadTail(String text, int width, FontMetrics fm) {
         String[] retarr = {"", ""};
@@ -85,104 +81,10 @@ public class DrawingUtils {
         return retarr;
     }
 
-    static public void drawLongStringBySpliting(Graphics2D gfx, String text, int x,
-                                                int y, int width, int height) {
-        FontMetrics fm = gfx.getFontMetrics(gfx.getFont());
-        int textHeight = fm.getHeight();
-        int textWidth = fm.stringWidth(text);
-
-        List<String> textParts = new ArrayList<>();
-
-        int textX = 8;
-        int textY;
-        int lineSpacing = 4;
-
-        if (textWidth > width) {
-            String[] headTail;
-            String head = text;
-            do {
-                headTail = getHeadTail(head, width, fm);
-                if (!"".equals(headTail[0])) {
-                    textParts.add(headTail[0]);
-                    head = headTail[1];
-                }
-            }
-            while (!"".equals(headTail[1]));
-            if (textParts.size() > 0) {
-                textY = y + (height - (textHeight + lineSpacing) * textParts.size() - lineSpacing) / 2 + fm.getAscent();
-
-                for (int i = 0; i < textParts.size(); i++) {
-                    gfx.drawString(textParts.get(i), textX, textY + i * textHeight + lineSpacing);
-                }
-            }
-        } else {
-//        textX = x + (width - textWidth) / 2;
-            textY = y + (height - textHeight) / 2 + fm.getAscent();
-            gfx.drawString(text, textX, textY);
-        }
-    }
-
-
-    static public void drawInk(Graphics2D gfx, PenData pd0, PenData pd1, EstimationQuestionForm estimationQuestionForm) {
-        gfx.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-                RenderingHints.VALUE_ANTIALIAS_ON);
-        gfx.setRenderingHint(RenderingHints.KEY_RENDERING,
-                RenderingHints.VALUE_RENDER_QUALITY);
-        gfx.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION,
-                RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
-        gfx.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING,
-                RenderingHints.VALUE_COLOR_RENDER_QUALITY);
-        gfx.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL,
-                RenderingHints.VALUE_STROKE_PURE);
-
-        int lineWidth = 5;
-        int pressure = pd0.getPressure();
-//            gfx.setColor(new Color(0, 0, 64, 255));
-        int trans = 255 * pressure / 1024 + 100;
-        trans = trans > 255 ? 255 : trans;
-        gfx.setColor(new Color(0, 0, 64, trans));
-
-        gfx.setStroke(new BasicStroke(lineWidth, BasicStroke.CAP_ROUND,
-                BasicStroke.JOIN_ROUND));
-
-        Point2D.Float pt0 = tabletToClient(pd0, estimationQuestionForm);
-        Point2D.Float pt1 = tabletToClient(pd1, estimationQuestionForm);
-        Shape l = new Line2D.Float(pt0, pt1);
-        gfx.draw(l);
-    }
-
-    static public void drawInk(Graphics2D gfx, List<PenData> penData, EstimationQuestionForm estimationQuestionForm) {
-        PenData[] pd = new PenData[0];
-        pd = penData.toArray(pd);
-        for (int i = 1; i < pd.length; ++i) {
-            if (pd[i - 1].getSw() != 0 && pd[i].getSw() != 0) {
-                drawInk(gfx, pd[i - 1], pd[i], estimationQuestionForm);
-            }
-        }
-    }
-
-    public static Point2D.Float tabletToClient(PenData penData, EstimationQuestionForm estimationQuestionForm) {
-        // Client means the panel coordinates.
-        return new Point2D.Float(
-                (float) penData.getX() * estimationQuestionForm.getPanel().getWidth() / estimationQuestionForm.getCapability().getTabletMaxX(),
-                (float) penData.getY() * estimationQuestionForm.getPanel().getHeight() / estimationQuestionForm.getCapability().getTabletMaxY());
-    }
-
     public static Point2D.Float tabletToScreen(PenData penData, EstimationQuestionForm estimationQuestionForm) {
         // Screen means LCD screen of the tablet.
         return new Point2D.Float(
                 (float) penData.getX() * estimationQuestionForm.getCapability().getScreenWidth() / estimationQuestionForm.getCapability().getTabletMaxX(),
                 (float) penData.getY() * estimationQuestionForm.getCapability().getScreenHeight() / estimationQuestionForm.getCapability().getTabletMaxY());
     }
-
-    public static Point clientToScreen(Point pt, EstimationQuestionForm estimationQuestionForm) {
-        // client (window) coordinates to LCD screen coordinates.
-        // This is needed for converting mouse coordinates into LCD bitmap
-        // coordinates as that's
-        // what this application uses as the coordinate space for buttons.
-        return new Point(
-                Math.round((float) pt.getX() * estimationQuestionForm.getCapability().getScreenWidth() / estimationQuestionForm.getPanel().getWidth()),
-                Math.round((float) pt.getY() * estimationQuestionForm.getCapability().getScreenHeight() / estimationQuestionForm.getPanel().getHeight()));
-    }
-
 }
--- src/main/java/com/ys/mfc/EstimationQuestionForm.java
+++ src/main/java/com/ys/mfc/EstimationQuestionForm.java
@@ -6,7 +6,6 @@ import com.WacomGSS.STU.STUException;
 import com.WacomGSS.STU.Tablet;
 import com.WacomGSS.STU.UsbDevice;
 
-import javax.swing.*;
 import java.awt.*;
 import java.awt.Rectangle;
 import java.awt.geom.Point2D;
@@ -33,18 +32,10 @@ public class EstimationQuestionForm implements ITabletHandler {
     private int headerHeight = 0;
     private AnswerButtonPressedListener answerButtonListener;
     private int pad = 4;
-    // The isDown flag is used like this:
-    // 0 = up
-    // +ve = down, pressed on button number
-    // -1 = down, inking
-    // -2 = down, ignoring
-    private int isDown;
+
     private List<PenData> penData; // Array of data being stored. This can
-    private JPanel panel;
-    private BufferedImage bitmap; // This bitmap that we display on the
-    // screen.
-    private EncodingMode encodingMode; // How we send the bitmap to the
-    // device.
+    private BufferedImage bitmap; // This bitmap that we display on the screen.
+    private EncodingMode encodingMode; // How we send the bitmap to the device.
     private byte[] bitmapData; // This is the flattened data of the bitmap
 
     public EstimationQuestionForm(UsbDevice usbDevice,
@@ -77,7 +68,6 @@ public class EstimationQuestionForm implements ITabletHandler {
         }
 
         this.headerHeight = this.capability.getScreenHeight() / 4;
-
         int offset = this.headerHeight;
 
         for (int i = 0; i < answerVariants.size(); i++) {
@@ -85,9 +75,6 @@ public class EstimationQuestionForm implements ITabletHandler {
             button.buttonType = ButtonType.ANSWERVARIANT;
             AnswerVariant answerVariant = answerVariants.get(i);
             button.id = answerVariant.getId();
-//            button.text = ("".equals(answerVariant.getAltTitle()) || null == answerVariant.getAltTitle())?
-//                    answerVariant.getAltTitle() :
-//                    answerVariant.getTitle();
             button.text = answerVariant.getAltTitle();
             if ("".equals(button.text)) button.text = answerVariant.getTitle();
             button.bounds = new Rectangle();
@@ -99,22 +86,6 @@ public class EstimationQuestionForm implements ITabletHandler {
             this.buttons.add(button);
         }
 
-
-        // Set the size of the client window to be actual size,
-        // based on the reported DPI of the monitor.
-
-//        int screenResolution = this.getToolkit().getScreenResolution();
-//        Dimension d = new Dimension(this.capability.getTabletMaxX()
-//                * screenResolution / 2540,
-//                this.capability.getTabletMaxY() * screenResolution
-//                        / 2540);
-
-//        this.panel.setPreferredSize(d);
-//        this.setLayout(new BorderLayout());
-//        this.setResizable(false);
-//        this.add(this.panel);
-//        this.pack();
-
         byte encodingFlag = ProtocolHelper.simulateEncodingFlag(
                 this.tablet.getProductId(),
                 this.capability.getEncodingFlag());
@@ -122,14 +93,9 @@ public class EstimationQuestionForm implements ITabletHandler {
         boolean useColor = ProtocolHelper
                 .encodingFlagSupportsColor(encodingFlag);
 
-        // Disable color if the bulk driver isn't installed.
-        // This isn't necessary, but uploading colour images with out
-        // the driver
-        // is very slow.
         useColor = useColor && this.tablet.supportsWrite();
 
-        // Calculate the encodingMode that will be used to update the
-        // image
+        // Calculate the encodingMode that will be used to update the image
         if (useColor) {
             if (this.tablet.supportsWrite())
                 this.encodingMode = EncodingMode.EncodingMode_16bit_Bulk;
@@ -180,40 +146,21 @@ public class EstimationQuestionForm implements ITabletHandler {
                         (int) btn.bounds.getY(),
                         (int) btn.bounds.getWidth(),
                         (int) btn.bounds.getHeight());
-//                DrawingUtils.drawCenteredString(gfx, btn.text,
                 DrawingUtils.drawLongStringBySpliting(gfx, btn.text,
                         (int) btn.bounds.getX(),
                         (int) btn.bounds.getY(),
                         (int) btn.bounds.getWidth(),
                         (int) btn.bounds.getHeight());
             });
-
             gfx.dispose();
         }
 
-        // Now the bitmap has been created, it needs to be converted to
-        // device-native
-        // format.
         this.bitmapData = ProtocolHelper.flatten(this.bitmap,
                 this.bitmap.getWidth(), this.bitmap.getHeight(),
                 useColor);
 
-        // If you wish to further optimize image transfer, you can
-        // compress the image using
-        // the Zlib algorithm.
-        boolean useZlibCompression = false;
-
-        if (!useColor && useZlibCompression) {
-            // m_bitmapData = compress_using_zlib(m_bitmapData); //
-            // insert compression here!
-            // m_encodingMode = EncodingMode.EncodingMode_1bit_Zlib;
-        }
-
         // Add the delagate that receives pen data.
         this.tablet.addTabletHandler(this);
-
-        // Initialize the screen
-        //clearScreen();
 
         // Enable the pen data on the screen (if not already)
         this.tablet.setInkingMode(InkingMode.Off);
@@ -223,9 +170,6 @@ public class EstimationQuestionForm implements ITabletHandler {
     public void setAnswerButtonListener(AnswerButtonPressedListener answerButtonListener) {
         this.answerButtonListener = answerButtonListener;
     }
-    // be subsequently used as desired.
-
-//    private Button[] btns; // The array of buttons that we are emulating.
 
     RectangleDimensions getAnswerButtonDimension() {
         int buttonCount = answerVariants.size();
@@ -237,34 +181,8 @@ public class EstimationQuestionForm implements ITabletHandler {
         return dim;
     }
 
-    private Point2D.Float tabletToClient(PenData penData) {
-        // Client means the panel coordinates.
-        return new Point2D.Float(
-                (float) penData.getX() * this.panel.getWidth() / this.capability.getTabletMaxX(),
-                (float) penData.getY() * this.panel.getHeight() / this.capability.getTabletMaxY());
-    }
-
-    private Point2D.Float tabletToScreen(PenData penData) {
-        // Screen means LCD screen of the tablet.
-        return new Point2D.Float(
-                (float) penData.getX() * this.capability.getScreenWidth() / this.capability.getTabletMaxX(),
-                (float) penData.getY() * this.capability.getScreenHeight() / this.capability.getTabletMaxY());
-    }
-
-    private Point clientToScreen(Point pt) {
-        // client (window) coordinates to LCD screen coordinates.
-        // This is needed for converting mouse coordinates into LCD bitmap
-        // coordinates as that's
-        // what this application uses as the coordinate space for buttons.
-        return new Point(
-                Math.round((float) pt.getX() * this.capability.getScreenWidth() / this.panel.getWidth()),
-                Math.round((float) pt.getY() * this.capability.getScreenHeight() / this.panel.getHeight()));
-    }
-    // that we send to the device.
-
     public void dispose() {
-        // Ensure that you correctly disconnect from the tablet, otherwise
-        // you are
+        // Ensure that you correctly disconnect from the tablet, otherwise you are
         // likely to get errors when wanting to connect a second time.
         if (this.tablet != null) {
             try {
@@ -275,26 +193,10 @@ public class EstimationQuestionForm implements ITabletHandler {
             this.tablet.disconnect();
             this.tablet = null;
         }
-
-//        super.dispose();
-    }
-
-    public JPanel getPanel() {
-        return panel;
     }
 
     public Capability getCapability() {
         return capability;
-    }
-
-    @Override
-    public void onGetReportException(STUException e) {
-
-    }
-
-    @Override
-    public void onUnhandledReportData(byte[] bytes) {
-
     }
 
     @Override
@@ -320,6 +222,42 @@ public class EstimationQuestionForm implements ITabletHandler {
             }
         }
     }
+
+    private enum ButtonType {
+        NAV, ANSWERVARIANT;
+    }
+
+    private static class RectangleDimensions {
+        int height = 0;
+        int widht = 0;
+    }
+
+    private class Button {
+        Rectangle bounds; // in Screen coordinates
+        String text;
+        ButtonType buttonType = ButtonType.NAV;
+        String id = "";
+//        ActionListener click = (e -> {
+//            EstimationQuestionForm.this.pressedButtonId = id;
+//            EstimationQuestionForm.this.answerButtonListener
+//                    .ansewrButtonPressed(new AnswerButtonPressedEvent(EstimationQuestionForm.this, "Button pressed id = " + id));
+//        });
+//
+//        void performClick() {
+////            actionPerformed(new AnswerButtonPressedEvent(this, "Нахата кнопка id = " + id));
+//        }
+    }
+
+    @Override
+    public void onGetReportException(STUException e) {
+
+    }
+
+    @Override
+    public void onUnhandledReportData(byte[] bytes) {
+
+    }
+
 
     @Override
     public void onPenDataOption(PenDataOption penDataOption) {
@@ -386,28 +324,4 @@ public class EstimationQuestionForm implements ITabletHandler {
 
     }
 
-    private enum ButtonType {
-        NAV, ANSWERVARIANT;
-    }
-
-    private static class RectangleDimensions {
-        int height = 0;
-        int widht = 0;
-    }
-
-    private class Button {
-        Rectangle bounds; // in Screen coordinates
-        String text;
-        ButtonType buttonType = ButtonType.NAV;
-        String id = "";
-//        ActionListener click = (e -> {
-//            EstimationQuestionForm.this.pressedButtonId = id;
-//            EstimationQuestionForm.this.answerButtonListener
-//                    .ansewrButtonPressed(new AnswerButtonPressedEvent(EstimationQuestionForm.this, "Button pressed id = " + id));
-//        });
-//
-//        void performClick() {
-////            actionPerformed(new AnswerButtonPressedEvent(this, "Нахата кнопка id = " + id));
-//        }
-    }
 }
